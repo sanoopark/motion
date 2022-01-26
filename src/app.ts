cdc41788326014fae905f0cb2b630ecd21db722b
sanoopark <sanoopark@gmail.com>
2022-01-26 23:39:59 +0900

    Feat: PageComponent 추가

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import "../style/style.css";
+import { PageComponent } from "./components/page";
+
+class App {
+  private readonly page: PageComponent;
+  constructor(appRoot: HTMLElement) {
+    this.page = new PageComponent();
+    this.page.attachTo(appRoot);
+  }
+}
+
+new App(document.querySelector(".document")! as HTMLElement);
