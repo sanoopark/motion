198540be81ecb4326ab7c53585d3b9ca7a707fa7
sanoopark <sanoopark@gmail.com>
2022-01-27 21:24:57 +0900

    Feat: ImageComponent 추가

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import "../style/style.css";
-import { PageComponent } from "./components/page";
+import { PageComponent } from "./components/page/page";
+import { ImageComponent } from "./components/page/item/image";
 
 class App {
   private readonly page: PageComponent;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
+
+    const image = new ImageComponent(
+      "Image Title",
+      "https://picsum.photos/600/300"
+    );
+    image.attachTo(appRoot, "beforeend");
   }
 }
 
