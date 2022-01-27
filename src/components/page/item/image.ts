c98d3c1bc2ca28f9bae00113b7b8704664408891
sanoopark <sanoopark@gmail.com>
2022-01-27 21:40:52 +0900

    Refactor: BaseComponent 추가

--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -1,16 +1,15 @@
-export class ImageComponent {
-  private element: HTMLElement;
+import { BaseComponent } from "./../../component";
+
+export class ImageComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
-    const template = document.createElement("template");
-    template.innerHTML = `
+    super(`
       <section class="image">
         <div class="image__holder">
           <img class="image__thumnail" />
           <p class="image__title"></p>
         </div>
       </section>
-    `;
-    this.element = template.content.firstElementChild! as HTMLElement;
+    `);
 
     const imageElement = this.element.querySelector(
       ".image__thumnail"
@@ -22,9 +21,5 @@ export class ImageComponent {
       ".image__title"
     )! as HTMLParagraphElement;
     titleElement.textContent = title;
-  }
-
-  attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
-    parent.insertAdjacentElement(position, this.element);
   }
 }
