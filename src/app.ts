dbac282da4aa596b471cea4ee705b832264c0e5d
sanoopark <sanoopark@gmail.com>
2022-01-27 22:15:46 +0900

    Feat: TodoComponent 추가

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import "../style/style.css";
 import { PageComponent } from "./components/page/page";
 import { ImageComponent } from "./components/page/item/image";
+import { NoteComponent } from "./components/page/item/note";
+import { TodoComponent } from "./components/page/item/todo";
 
 class App {
   private readonly page: PageComponent;
@@ -13,6 +15,12 @@ class App {
       "https://picsum.photos/600/300"
     );
     image.attachTo(appRoot, "beforeend");
+
+    const note = new NoteComponent("Note Title", "Note Body");
+    note.attachTo(appRoot, "beforeend");
+
+    const todo = new TodoComponent("Todo Title", "Todo Item");
+    todo.attachTo(appRoot, "beforeend");
   }
 }
 
