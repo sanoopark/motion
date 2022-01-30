2672cac5bfae5bd5c5a717f6a2953ae2203d8bf6
sanoopark <sanoopark@gmail.com>
2022-01-30 20:50:43 +0900

    Feat: 빈 InputDialog 추가

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,19 @@
 import "../style/style.css";
-import { Component } from "./components/component";
-import { ImageComponent } from "./components/page/item/image";
+import { InputDialog } from "./components/dialog/dialog";
 import { VideoComponent } from "./components/page/item/video";
-import { NoteComponent } from "./components/page/item/note";
 import { TodoComponent } from "./components/page/item/todo";
+import { NoteComponent } from "./components/page/item/note";
+import { ImageComponent } from "./components/page/item/image";
 import {
   Composable,
   PageComponent,
   PageItemComponent,
 } from "./components/page/page";
+import { Component } from "./components/component";
 
 class App {
   private readonly page: Component & Composable;
+
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent(PageItemComponent);
     this.page.attachTo(appRoot);
@@ -24,7 +26,7 @@ class App {
 
     const video = new VideoComponent(
       "Video Title",
-      "https://youtu.be/5qap5aO4i9A"
+      "https://youtu.be/K3-jG52XwuQ"
     );
     this.page.addChild(video);
 
@@ -33,6 +35,23 @@ class App {
 
     const todo = new TodoComponent("Todo Title", "Todo Item");
     this.page.addChild(todo);
+
+    const imageBtn = document.querySelector("#new-image")! as HTMLButtonElement;
+    imageBtn.addEventListener("click", () => {
+      const dialog = new InputDialog();
+      const root = document.querySelector("#root")! as HTMLElement;
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(root);
+      });
+
+      dialog.setOnSubmitListener(() => {
+        // 섹션을 만들어서 페이지에 추가 해준다
+        dialog.removeFrom(root);
+      });
+
+      dialog.attachTo(root, "beforeend");
+    });
   }
 }
 
